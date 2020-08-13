c8f10f05c5d4855535d4d8b0a7c19db156cbbb7c
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-08-12 22:47:20 -0500

    connect MongoDB to app

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API ...' })
 );
-
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
