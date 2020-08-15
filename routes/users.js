2cbb7cb1a34e9b9b8f9ee8e1716b3da6e0cde0ec
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-08-14 23:47:06 -0500

    find email

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,17 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed')
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if(user) {
+        return res.status(400).json({ msg: 'User already exists'});
+      }
+    } catch(err) {
+      
+    }
   }
 );
 
