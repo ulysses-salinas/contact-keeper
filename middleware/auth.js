47aa69050947619690db28a27805de8a694321d7
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-08-23 17:34:42 -0500

    verify jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,4 +9,11 @@ module.exports = function (req, res, next) {
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
+
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {}
 };
