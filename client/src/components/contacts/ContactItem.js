71bc8d146495fe2c07a3d7ea75cb29f2ec54802e
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-09-03 21:47:27 -0500

    useContext onDelete

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact } = contactContext;
+
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+  };
   return (
     <div className='card bg-light'>
       <h3 className='text-primary text-left'>
@@ -18,23 +25,29 @@ const ContactItem = ({ contact }) => {
           {type}
         </span>
       </h3>
-      <ul className="list">
-          {email && (<li>
-              <i className="fas fa-envelope-open"></i> {email}
-          </li>)}
-          {phone && (<li>
-              <i className="fas fa-phone"></i> {phone}
-          </li>)}
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open'></i> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'></i> {phone}
+          </li>
+        )}
       </ul>
       <p>
-          <button className="btn btn-dark btn-sm">Edit</button>
-          <button className="btn btn-danger btn-sm">Delete</button>
+        <button className='btn btn-dark btn-sm'>Edit</button>
+        <button className='btn btn-danger btn-sm' onClick={onDelete}>
+          Delete
+        </button>
       </p>
     </div>
   );
 };
 
 ContactItem.propTypes = {
-    contact: PropTypes.object.isRequired,
-}
+  contact: PropTypes.object.isRequired,
+};
 export default ContactItem;
