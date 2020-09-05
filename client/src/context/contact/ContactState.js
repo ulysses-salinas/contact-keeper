22e9ff72109be7cdbb1b31a8dd525bf84ea32b4a
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-09-04 22:04:57 -0500

    set and clear current contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,6 +37,8 @@ const ContactState = props => {
         type: 'professional',
       },
     ],
+
+    current: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -52,9 +54,13 @@ const ContactState = props => {
   };
 
   // Set Current Contact
-
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
   // Clear Current Contact
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
   // Update Contact
 
   // Filter Contacts
@@ -65,8 +71,11 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
         deleteContact,
+        setCurrent,
+        clearCurrent,
       }}
     >
       {props.children}
