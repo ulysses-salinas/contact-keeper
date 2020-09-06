0c331323659a678462614a25a18a0a703f5fe7c3
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-09-05 20:02:46 -0500

    updateContact function

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -62,7 +62,9 @@ const ContactState = props => {
     dispatch({ type: CLEAR_CURRENT });
   };
   // Update Contact
-
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
   // Filter Contacts
 
   // Clear Filter
@@ -76,6 +78,7 @@ const ContactState = props => {
         deleteContact,
         setCurrent,
         clearCurrent,
+        updateContact,
       }}
     >
       {props.children}
