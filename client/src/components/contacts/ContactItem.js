9ad44ddaf033ff757b474845ec1cd6289104852e
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-09-07 13:39:45 -0500

    toUppercase type

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -23,7 +23,7 @@ const ContactItem = ({ contact }) => {
             (type === 'professional' ? 'badge-success' : 'badge-primary')
           }
         >
-          {type}
+          {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
       <ul className='list'>
