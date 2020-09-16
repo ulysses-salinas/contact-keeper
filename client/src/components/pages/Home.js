803bfef5fdf0be47c94893bd05a87a41a1a5a1bf
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-09-15 21:23:52 -0500

    load user and set token

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,6 +7,7 @@ import AuthContext from '../../context/auth/authContext';
 const Home = () => {
   const authContext = useContext(AuthContext);
 
+  
   useEffect(() => {
     authContext.loadUser();
     // eslint-disable-next-line
